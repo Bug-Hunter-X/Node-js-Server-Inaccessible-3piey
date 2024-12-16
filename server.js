e9d9bbc498027c5677408e132692355161d99e84
BugHunter-X <askzenai@gmail.com>
2024-12-16 06:06:06 +0100

    Add server.js file

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello, World!');
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(8080);
+console.log('Server is running on port 8080');
+
+//Error:The server may not be accessible due to incorrect port or firewall settings.
